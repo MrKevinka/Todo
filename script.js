18aae03a85fe940af55dab0942ad6717a091a756
nitamathur <nitamathur33@gmail.com>
2023-08-24 16:20:53 +0200

    update

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // mamoon part
 
 // nita part
+document.addEventListener('DOMContentLoaded', function() {
+    const taskText = document.querySelector('.task-text');
+    const editButton = document.querySelector('.edit-button');
 
+    editButton.addEventListener('click', function() {
+        const currentText = taskText.textContent;
+        const newText = prompt('Edit the task:', currentText);
+
+        if (newText !== null && newText !== '') {
+            taskText.textContent = newText;
+        }
+    });
+});
 // vaishali part
 
 // kevin part
